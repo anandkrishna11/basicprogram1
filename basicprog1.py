360b92d9a5c0e6cfb05b5ef20b31d9405b934b7e
anandkrishna11 <anandkrishna2005ma@gmail.com>
2024-10-18 14:32:49 +0530

    Add files via upload

---
+++ basicprog1.py
@@ -0,0 +1,8 @@
+'''author:anand
+date:18-10-2024
+discription:program that asks the user to input their name and age.after reciving the input,message that includes the user's name and age
+output example:Hello, John! You are 25 years old'''
+name=input("enter your name:")
+age=int(input("enter your age"))
+print("hello",name+"! you are",age,"years old")
+
