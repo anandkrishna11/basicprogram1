43ba51bdc87f6f5c704ec20bb66e11867364c5c0
anandkrishna11 <anandkrishna2005ma@gmail.com>
2024-10-18 16:31:31 +0530

    Add files via upload

---
+++ stringDemo2.py
@@ -0,0 +1,10 @@
+'''name:Anand Krishna M A
+discription:Python program to print the following string in a specific format (see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+output example:Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are'''
+print("Twinkle,\ttwinkle,\tlittle star,\n\t\tHow i wonder what you are! \n\t\t\tUp above the world so high,\n\t\t\t Like a diamond in the sky,\ntwinkle twinkle little star, \n\t\thow i wonder what you are")
