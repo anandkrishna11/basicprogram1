a3b850d0bcf711894e66683a659959739b0698dc
anandkrishna11 <anandkrishna2005ma@gmail.com>
2024-10-18 15:08:44 +0530

    Add files via upload

---
+++ stringdemo.py
@@ -0,0 +1,9 @@
+'''author:anand krishna ma
+date:18-10-2024
+discription:Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+str_var1="Hello, World!"
+str_var2=str_var1[7:12]
+str_var3=" Everyone!"
+str_var4=str_var2+str_var3
+print(str_var4)
