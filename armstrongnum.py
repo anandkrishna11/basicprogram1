4f532a64d01bb5f1f89c04d6938892c979c61af3
anandkrishna11 <anandkrishna2005ma@gmail.com>
2024-10-25 16:21:47 +0530

    Add files via upload

---
+++ armstrongnum.py
@@ -0,0 +1,18 @@
+'''Author:Anand Krishna M A
+Date:25-10-2024
+description:program to check weather the number is armstrong or not'''
+
+number=int(input("Enter the number"))
+armstrong=0
+number2=number
+while number2!=0:
+    num=(number2%10)**3
+    armstrong+=num
+    number2=number2//10
+if number==armstrong:
+    print(number,"is an armstrong number!")
+else:
+    print("oops",number,"is not an armstrong number!")
+
+#end
+
