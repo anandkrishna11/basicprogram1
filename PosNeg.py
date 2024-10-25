bf548f14ab5351f88bdf7c2e2a86394d94554082
anandkrishna11 <anandkrishna2005ma@gmail.com>
2024-10-25 14:30:15 +0530

    Add files via upload

---
+++ PosNeg.py
@@ -0,0 +1,11 @@
+"""author:Anand Krishna M A
+date:25-10-2024
+Discription:Program to check weather the number is positive or negetive"""
+
+num=int(input("Enter a number:"))
+if num>0:
+    print(num,"is a positive digit")
+elif num<0:
+    print(num,"is a negetive digit")
+else:
+    print("you entered 0")
