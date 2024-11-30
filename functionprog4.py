8166375c4654276c7cc1b7d7578c6a25e7edc847
anandkrishna11 <anandkrishna2005ma@gmail.com>
2024-11-30 15:37:17 +0530

    Add files via upload

---
+++ functionprog4.py
@@ -0,0 +1,8 @@
+'''author:Anand Krishna M A
+Description:Write a Python program to reverse a string.
+Date:30-11-2024'''
+
+def reverse_str(x):
+    print(x[-1::-1])
+str1=input("Enter the string:")
+reverse_str(str1)
